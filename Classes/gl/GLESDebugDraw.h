d122879a53c86e3b648c6457942bec195310fc26
Marco Wang <m.aesophor@gmail.com>
2019-05-11 13:55:48 +0800

    gl: lint

--- Classes/gl/GLESDebugDraw.h
+++ Classes/gl/GLESDebugDraw.h
@@ -34,15 +34,15 @@ class GLESDebugDraw : public b2Draw {
   GLESDebugDraw(float32 ratio);
   virtual ~GLESDebugDraw() = default;
 
-  virtual void DrawPolygon(const b2Vec2* vertices, int vertexCount, const b2Color& color);
-  virtual void DrawSolidPolygon(const b2Vec2* vertices, int vertexCount, const b2Color& color);
-  virtual void DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color);
-  virtual void DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color);
-  virtual void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color);
-  virtual void DrawTransform(const b2Transform& xf);
-  virtual void DrawPoint(const b2Vec2& p, float32 size, const b2Color& color);
-  virtual void DrawString(int x, int y, const char* string, ...); 
-  virtual void DrawAABB(b2AABB* aabb, const b2Color& color);
+  void DrawPolygon(const b2Vec2* vertices, int vertexCount, const b2Color& color);
+  void DrawSolidPolygon(const b2Vec2* vertices, int vertexCount, const b2Color& color);
+  void DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color);
+  void DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color);
+  void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color);
+  void DrawTransform(const b2Transform& xf);
+  void DrawPoint(const b2Vec2& p, float32 size, const b2Color& color);
+  void DrawString(int x, int y, const char* string, ...); 
+  void DrawAABB(b2AABB* aabb, const b2Color& color);
 
  private:
   void initShader();
