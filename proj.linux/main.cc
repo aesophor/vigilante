9d4a856a8a8ea57f7c4051ad8c427395050809bd
Marco Wang <m.aesophor@gmail.com>
2019-08-01 23:52:37 +0800

    proj.linux: main: remove redundant aliases

--- proj.linux/main.cc
+++ proj.linux/main.cc
@@ -6,10 +6,6 @@
 #include "../Classes/AppDelegate.h"
 #include "../Classes/util/Logger.h"
 
-using std::cerr;
-using std::endl;
-using cocos2d::Application;
-
 int main(int argc, char* args[]) {
   // Install SIGSEGV handler. See util/Logger.cc
   signal(SIGSEGV, &vigilante::logger::segvHandler);
@@ -18,9 +14,9 @@ int main(int argc, char* args[]) {
   AppDelegate app;
 
   try {
-    return Application::getInstance()->run();
+    return cocos2d::Application::getInstance()->run();
   } catch (const std::exception& ex) {
-    cerr << ex.what() << endl;
+    std::cerr << ex.what() << std::endl;
     return EXIT_FAILURE;
   }
 
