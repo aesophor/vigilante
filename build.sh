405280e73cc5c6411dbda2eafd5d326e1c136077
Marco Wang <m.aesophor@gmail.com>
2020-01-12 13:56:19 +0800

    build.sh: export ENV variable in build script instead of .zshrc

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 # Copyright (c) 2019 Marco Wang <m.aesophor@gmail.com>. All rights reserved.
-
 #!/usr/bin/env sh
+
+# Add environment variable COCOS_CONSOLE_ROOT for cocos2d-x
+export COCOS_CONSOLE_ROOT="/home/aesophor/Code/cocos2d-x-3.17.1/tools/cocos2d-console/bin"
+export PATH=$COCOS_CONSOLE_ROOT:$PATH
+
+# Add environment variable COCOS_TEMPLATES_ROOT for cocos2d-x
+export COCOS_TEMPLATES_ROOT="/home/aesophor/Code/cocos2d-x-3.17.1/templates"
+export PATH=$COCOS_TEMPLATES_ROOT:$PATH
 
 project_root="/home/aesophor/Code/Vigilante/"
 spritesheets_list="Resources/Texture/spritesheets.txt"
 quests_list="Resources/Gameplay/quests_list.txt"
+
 
 # Generate Resources/Texture/spritesheets.txt
 cd $project_root/Resources && find Texture -type f | grep plist > $project_root/$spritesheets_list
