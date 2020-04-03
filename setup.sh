40022a21f398b1ebe5b6fb892ef0c21c4cb3be82
Marco Wang <m.aesophor@gmail.com>
2020-04-03 16:58:20 +0800

    Download dependencies via setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# Copyright (c) 2019 Marco Wang <m.aesophor@gmail.com>. All rights reserved.
+#!/usr/bin/env sh
+
+# Download cocos2d-x-3.17.1
+wget -O cocos2d-x-3.17.1.tar.gz https://digitalocean.cocos2d-x.org/Cocos2D-X/cocos2d-x-3.17.1.zip
+unzip cocos2d-x-3.17.1.tar.gz
