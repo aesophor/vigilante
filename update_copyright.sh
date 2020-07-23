b94649be0826cd1232e4d4510b805dc8f587f337
Marco Wang <m.aesophor@gmail.com>
2020-07-23 11:13:30 +0800

    Remove .clang-format and lint.sh; Add new update_copyright.sh

---
+++ update_copyright.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# Update copyright
+year_begin=2018
+year_current=`date +"%Y"`
+copyright="\/\/ Copyright (c) ${year_begin}-${year_current} Marco Wang <m.aesophor@gmail.com>.\
+ All rights reserved."
+
+find src -type f -exec sed -i "1s/.*/${copyright}/"
