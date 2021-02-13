4f85b40ac79138276125ecc0978f86e32f4a99f3
Marco Wang <m.aesophor@gmail.com>
2021-02-13 14:57:12 +0800

    ui: control_hints: Add control hints

---
+++ src/ui/control_hints/ControlHints.cc
@@ -0,0 +1,126 @@
+// Copyright (c) 2018-2021 Marco Wang <m.aesophor@gmail.com>. All rights reserved.
+#include "ControlHints.h"
+
+#include <algorithm>
+
+#include "AssetManager.h"
+#include "util/KeyCodeUtil.h"
+#include "util/Logger.h"
+
+#define CONTROL_HINTS_Y 32
+
+using std::string;
+using cocos2d::Vec2;
+using cocos2d::Size;
+using cocos2d::Layer;
+using cocos2d::Label;
+using cocos2d::Director;
+using cocos2d::EventKeyboard;
+using cocos2d::ui::Layout;
+using cocos2d::ui::ImageView;
+using vigilante::asset_manager::kRegularFont;
+using vigilante::asset_manager::kRegularFontSize;
+using vigilante::asset_manager::kControlHints;
+
+namespace vigilante {
+
+const int ControlHints::_kHintGap = 25;
+const int ControlHints::Hint::_kIconLabelGap = 4;
+
+ControlHints* ControlHints::getInstance() {
+  static ControlHints instance;
+  return &instance;
+}
+
+ControlHints::ControlHints()
+    : _layer(Layer::create()),
+      _hints() {
+  _layer->setPositionY(CONTROL_HINTS_Y);
+
+  //show(EventKeyboard::KeyCode::KEY_CAPITAL_E, "Talk");
+  //show(EventKeyboard::KeyCode::KEY_UP_ARROW, "Open Door");
+}
+
+
+void ControlHints::show(const EventKeyboard::KeyCode keyCode, const string& text) {
+  if (_hints.size() >= 3) {
+    VGLOG(LOG_WARN, "Unable to add more control hints! Currently have 3.");
+    return;
+  }
+
+  _hints.push_back(ControlHints::Hint(keyCode, text));
+  _layer->addChild(_hints.back().getLayout());
+
+  normalize();
+}
+
+void ControlHints::hide(const EventKeyboard::KeyCode keyCode) {
+  if (_hints.empty()) {
+    VGLOG(LOG_WARN, "Unable to remove any more control hints! Currently have 0.");
+    return;
+  }
+
+  _hints.erase(std::remove_if(_hints.begin(),
+                              _hints.end(),
+                              [this, keyCode](const ControlHints::Hint& hint) {
+                                  return keyCode == hint.getKeyCode() &&
+                                    (_layer->removeChild(hint.getLayout()), true);
+                              }),
+               _hints.end());
+
+  normalize();
+}
+
+void ControlHints::normalize() {
+  if (_hints.empty()) {
+    return;
+  }
+
+  auto winSize = Director::getInstance()->getWinSize();
+  float nextX = winSize.width;
+
+  for (int i = _hints.size() - 1; i >= 0; i--) {
+    nextX -= _hints.at(i).getContentSize().width;
+    nextX -= _kHintGap;
+    _hints.at(i).getLayout()->setPositionX(nextX);
+  }
+}
+
+
+Layer* ControlHints::getLayer() const {
+  return _layer;
+}
+
+
+ControlHints::Hint::Hint(const EventKeyboard::KeyCode keyCode, const string& text)
+    : _layout(Layout::create()),
+      _icon(ImageView::create(kControlHints + keycode_util::keyCodeToString(keyCode) + ".png")),
+      _label(Label::createWithTTF(text, kRegularFont, kRegularFontSize)),
+      _keyCode(keyCode) {
+
+  _icon->setAnchorPoint({0, 1});
+  _label->setAnchorPoint({0, 1});
+  _label->setPositionX(_icon->getContentSize().width + _kIconLabelGap);
+  _label->getFontAtlas()->setAliasTexParameters();
+
+  _layout->setLayoutType(Layout::Type::HORIZONTAL);
+  _layout->addChild(_icon);
+  _layout->addChild(_label);
+}
+
+
+Size ControlHints::Hint::getContentSize() const {
+  const Size& iconSize = _icon->getContentSize();
+  const Size& labelSize = _label->getContentSize();
+  return {iconSize.width + labelSize.width, std::max(iconSize.height, labelSize.height)};
+}
+
+Layout* ControlHints::Hint::getLayout() const {
+  return _layout;
+}
+
+EventKeyboard::KeyCode ControlHints::Hint::getKeyCode() const {
+  return _keyCode;
+}
+
+}  // namespace vigilante
---
+++ src/ui/control_hints/ControlHints.h
@@ -0,0 +1,56 @@
+// Copyright (c) 2018-2021 Marco Wang <m.aesophor@gmail.com>. All rights reserved.
+#ifndef VIGILANTE_CONTROL_HINTS_H_
+#define VIGILANTE_CONTROL_HINTS_H_
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#include <cocos2d.h>
+#include <2d/CCLabel.h>
+#include <ui/UILayout.h>
+#include <ui/UIImageView.h>
+
+namespace vigilante {
+
+class ControlHints {
+ public:
+  static ControlHints* getInstance();
+  virtual ~ControlHints() = default;
+
+  void show(const cocos2d::EventKeyboard::KeyCode keyCode, const std::string& text);
+  void hide(const cocos2d::EventKeyboard::KeyCode keyCode);
+
+  cocos2d::Layer* getLayer() const;
+
+ private:
+  class Hint final {
+   public:
+    Hint(const cocos2d::EventKeyboard::KeyCode keyCode, const std::string& text);
+    ~Hint() = default;
+
+    cocos2d::Size getContentSize() const;
+    cocos2d::ui::Layout* getLayout() const;
+    cocos2d::EventKeyboard::KeyCode getKeyCode() const;
+
+   private:
+    static const int _kIconLabelGap;
+
+    cocos2d::ui::Layout* _layout;
+    cocos2d::ui::ImageView* _icon;
+    cocos2d::Label* _label;
+    cocos2d::EventKeyboard::KeyCode _keyCode;
+  };
+
+  ControlHints();
+  void normalize();
+
+  static const int _kHintGap;
+
+  cocos2d::Layer* _layer;
+  std::vector<ControlHints::Hint> _hints;
+};
+
+}  // namespace vigilante
+
+#endif  // VIGILANTE_CONTROL_HINTS_H_
