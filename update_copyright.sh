df89d97e704e66db84aa7aad4bdb4bf0f6de00fa
Marco Wang <m.aesophor@gmail.com>
2021-12-14 19:59:22 +0800

    Remove the obsolete update_copyright.sh
    Signed-off-by: Marco Wang <m.aesophor@gmail.com>

--- update_copyright.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# Update copyright
-year_begin=2018
-year_current=`date +"%Y"`
-copyright="\/\/ Copyright (c) ${year_begin}-${year_current} Marco Wang <m.aesophor@gmail.com>.\
- All rights reserved."
-
-find src -type f | xargs sed -i "1s/.*/${copyright}/"
