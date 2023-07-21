a75af1c3b341a401fdd828e21947d7642200e165
Marco Wang <m.aesophor@gmail.com>
2023-07-22 02:07:02 +0800

    util/ds: Introduce Digraph
    Signed-off-by: Marco Wang <m.aesophor@gmail.com>

--- Source/combat/ComboSystem.h
+++ Source/combat/ComboSystem.h
@@ -2,58 +2,12 @@
 #ifndef VIGILANTE_COMBO_SYSTEM_H_
 #define VIGILANTE_COMBO_SYSTEM_H_
 
-#include <list>
-#include <optional>
-#include <unordered_map>
-
 #include "character/Character.h"
+#include "util/ds/Digraph.h"
 
 namespace vigilante {
 
 class Character;
-
-template <typename V, typename E>
-class Digraph final {
- public:
-  using VertexId = std::size_t;
-  using VertexType = V;
-  using EdgeType = E;
-
-  VertexId addVertex(V vertex) {
-    const VertexId newVertexId = _vertices.size();
-    _vertices.emplace_back(std::move(vertex));
-    return newVertexId;
-  }
-
-  bool addOutgoingEdge(const VertexId id, E edge) {
-    const std::optional<V> vertex = getVertexById(id);
-    if (!vertex.has_value()) {
-      return false;
-    }
-    _outgoingEdges[id].emplace_back(std::move(edge));
-    return true;
-  }
-
-  std::optional<V> getVertexById(const VertexId id) {
-    if (id >= _vertices.size()) {
-      return std::nullopt;
-    }
-    return _vertices[id];
-  }
-
-  std::optional<std::list<E>> getOutgoingEdges(const VertexId src) const {
-    const auto it = _outgoingEdges.find(src);
-    if (it == _outgoingEdges.end()) {
-      return std::nullopt;
-    }
-
-    return it->second;
-  }
-
- private:
-  std::vector<V> _vertices;
-  std::unordered_map<VertexId, std::list<E>> _outgoingEdges;
-};
 
 template <typename S, typename T>
 class TimedFiniteStateMachine final {
